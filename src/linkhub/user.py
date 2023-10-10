ef5c1c5e8a284bc9bcd32bad5119cf769adb93bb
Paul John <rainson.work@gmail.com>
2023-10-10 23:28:01 +0300

    Add user module to the linkhub package
    The 'user' module defines a 'User' class which stores
    LinkHub user credentials and handles password hashing using
    bcrypt module and store the hash to the database.

--- src/linkhub/user.py
+++ src/linkhub/user.py
@@ -14,8 +14,53 @@ Key Features:
 
 Author: Paul John
 """
+import bcrypt
+from sqlalchemy.orm import relationship
+
+from linkhub.repository import Repository
 from linkhub.linkhub_base import LinkHubBase, Base
 
 
 class User(LinkHubBase, Base):
-    pass
+    """Defines a User class for managing user data"""
+    __tablename__ = "users"
+    username = Column(String(25), unique=True, nullable=False)
+    email = Column(String(60), unique=True, nullable=False)
+    __password = Column("password", String(60), nullable=False)
+    bio = Column(String(256), nullable=True)
+    repositories = relationship('Repository', backref='user')
+
+    def __init__(self, username, email, password, bio=None):
+        """Initializes an instance of User class"""
+        self.username = username
+        self.email = email
+        self.bio = bio
+        self.password = password
+
+    @property
+    def password(self):
+        """Getter for the password property"""
+        return self.__password
+
+    @password.setter
+    def password(self, new_password):
+        """Setter for the password property
+
+        Args:
+            new_password (str): password attribute
+        Returns:
+            str: hashed password
+        """
+        # Hashes the provided password and stores it
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), salt)
+        self.__password = hashed_password
+
+    def check_password(self, password):
+        """Verify a provided password against the stored hash"""
+        return bcrypt.checkpw(password.encode('utf-8'), self.__password)
+
+    def change_password(self, new_password):
+        """Change the user's password to a new one"""
+        # This will automatically call the setter
+        self.password = new_password
