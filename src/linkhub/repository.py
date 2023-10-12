09c460167db5bd3e21ec780f9bbffc10f8902871
Paul John <rainson.work@gmail.com>
2023-10-12 23:11:42 +0300

    Add repository module to linkhub package
    The repository module defines a Repository class used to manage
    resources/links defined int the resource module

--- src/linkhub/repository.py
+++ src/linkhub/repository.py
@@ -20,8 +20,43 @@ Usage Example:
 
 Author: Paul John
 """
+from sqlalchemy.orm import Table
+from sqlalchemy.orm import ForeignKey
+from sqlalchemy.orm import relationship
+
+from linkhub.tag import Tag
+from linkhub.resource import Resource
 from linkhub.linkhub_base import LinkHubBase, Base
 
 
+# Create an association table for repository and tags
+repository_tag_association = Table(
+        'repository_tag_association',
+        Base.metadata,
+        Column('repository_id', String(36), ForeignKey('repositories.id')),
+        Column('tag_id', String(36), ForeignKey('tags.id'))
+        )
+
+
 class Repository(LinkHubBase, Base):
-    pass
+    """Defines 'Repository' class for organizing and managing resources"""
+    __tablename__ = 'repositories'
+    name = Column(String(60), unique=True, nullable=False)
+    description = Column(String(255), nullable=True)
+    user_id = Column(String(36), ForeignKey('users.id'))
+    resources = relationship('Resource', backref='repository')
+    tags = relationship('Tag', secondary=repository_tag_association,
+                        backref='repositories')
+
+    def __init__(self, name, description=None, *args, **kwargs):
+        """Initializes an instance of Repository class
+
+        Args:
+            name (str): The name of the repository
+            description (str): The description of the repository
+            *args: Additional non-keyword arguments.
+            **kwargs: Additional keyword arguments.
+        """
+        super().__init__(*args, **kwargs)
+        self.name = name
+        self.description = description
