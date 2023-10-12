54a5d919272730ee168e51b23975d2bc4aaf7569
Paul John <rainson.work@gmail.com>
2023-10-12 23:13:04 +0300

    Add resource module to linkhub package
    The resource module define a Resource class used to create, define
    and manage resources/links. Resources can be managed using tags defined
    in the Tag class of tag module.

--- src/linkhub/resource.py
+++ src/linkhub/resource.py
@@ -13,8 +13,41 @@ Key Features:
 
 Author: Paul John
 """
+from sqlalchmey.orm import Table
+from sqlalchemy.orm import Foreignkey
+from sqlalchemy.orm import relationship
+
+from linkhub.tag import Tag
 from linkhub.linkhub_base import LinkHubBase, Base
 
 
+# Create an association table for resources and tags
+resource_tag_association = Table(
+        'resource_tag_association',
+        Base.metadata,
+        Column('resource_id', String(36), ForeignKey('resources.id')),
+        Column('tag_id', String(36), ForeignKey('tags.id'))
+        )
+
+
 class Resource(LinkHubBase, Base):
-    pass
+    """Defines a 'Resource' class for creating link resources"""
+    __tablename__ = 'resources'
+    title = Column(String(128), nullable=False)
+    url = Column(String(255), nullable=False)
+    repository_id = Column(String(36), ForeignKey('repository.id'))
+    tags = relationship('Tag', secondary=resource_tag_association,
+                        backref='resources')
+
+    def __init__(self, title, url, *args, **kwargs):
+        """Initializes an instance of Resource class
+
+        Args:
+            title (str): The title of the resource/link
+            url (str): The URL to the resource
+            *args: Additional non-keyword arguments.
+            **kwargs: Additional keyword arguments.
+            """
+        super().__init__(*args, **kwargs)
+        self.title = title
+        self.url = url
