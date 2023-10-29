fc7c6aa4e21f4ddc9cb5cb6a486a094410c05bda
Paul John <rainson.work@gmail.com>
2023-10-29 14:27:12 +0300

    Modify logging module

--- src/api/logging.py
+++ src/api/logging.py
@@ -1,3 +1,6 @@
+"""
+Logging mechanism
+"""
 import os
 import logging
 import traceback
@@ -8,7 +11,9 @@ from api import mail
 from flask import current_app
 from flask_mail import Message
 
+
 class Logging:
+    """Logging class"""
     def __init__(self):
         self.logger = self.setup_logger()
 
@@ -42,19 +47,19 @@ class Logging:
 
         if send_email:
             # Send error over email
-                if current_app.config.get('MAIL_USERNAME'):
-                    subject = 'LinkHub API Failure'
-                    sender = 'no-reply@linkhub.com'
-                    recipients = current_app.config['ADMINS']
+            if current_app.config.get('MAIL_USERNAME'):
+                subject = 'LinkHub API Failure'
+                sender = 'no-reply@linkhub.com'
+                recipients = current_app.config['ADMINS']
 
-                    # Convert the exception to a string with the full stack trace
-                    error_message = f"{str(error)}\n\n{traceback.format_exc()}"
+                # Convert the exception to a string with the full stack trace
+                error_message = f"{str(error)}\n\n{traceback.format_exc()}"
 
-                    msg = Message(subject, sender=sender, recipients=recipients)
-                    msg.body = error_message
+                msg = Message(subject, sender=sender, recipients=recipients)
+                msg.body = error_message
 
-                    try:
-                        mail.send(msg)
-                        self.logger.info('Error sent via email')
-                    except Exception as e:
-                        self.logger.info(f'Error sending email: {str(e)}')
+                try:
+                    mail.send(msg)
+                    self.logger.info('Error sent via email')
+                except Exception as e:
+                    self.logger.info(f'Error sending email: {str(e)}')
