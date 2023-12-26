968c20fe980f28c82d45563fcad6b4e637b7982e
Paul John <rainson.work@gmail.com>
2023-12-26 14:51:54 +0300

    update package

--- src/api/blueprints/__init__.py
+++ src/api/blueprints/__init__.py
@@ -1,11 +1,7 @@
 from flask import Blueprint
 
 # Instantiate API endpoints blueprint
-<<<<<<< HEAD
 endpoints = Blueprint('endpoints', __name__)
-=======
-endpoints = Blueprint("endpoints", __name__, url_prefix="/api")
->>>>>>> d46fa1ea02bc90ea106c63969d6bbfe19ab600d6
 
 # Import endpoints blueprint routes from modules
 from api.blueprints import init
